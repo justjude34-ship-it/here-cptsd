1ca6db1704ee569152fa5785bd4af7861f7716db
justjude34-ship-it <justjude34@gmail.com>
2026-09-03 13:21:46 +1000

    Add full Here HTML and service worker assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "here-v4";
-const ASSETS = ["./index.html", "./manifest.json", "./icon.svg"];
+const ASSETS = ["./index.html", "./manifest.json", "./icon.svg", "./app.js"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
